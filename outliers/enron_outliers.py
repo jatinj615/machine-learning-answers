59c3506b516f78f0ccf7fb3606073415f498e444
Jatin <jatinj615@gmail.com>
2017-11-03 14:03:51 +0530

    outliers final

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -8,12 +8,51 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
+outliers = []
+for key in data_dict:
+    val = data_dict[key]['salary']
+    if val == 'NaN':
+        continue 
+    
+    outliers.append((key,int(val)))
+    
+outliers_final = (sorted(outliers,key=lambda x:x[1],reverse=True)[:2])
+print(outliers_final)
+data_dict.pop('TOTAL',0)
+
+import matplotlib.pyplot as plt
+data = featureFormat(data_dict, features)
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    plt.scatter(salary,bonus)
+    
+plt.xlabel('salary')
+plt.ylabel('bonus')
+plt.show()
+outliers = []
+for key in data_dict:
+    val = data_dict[key]['salary']
+    if val == 'NaN':
+        continue 
+    
+    outliers.append((key,int(val)))
+    
+outliers_final = (sorted(outliers,key=lambda x:x[1],reverse=True)[:2])
+print(outliers_final)
