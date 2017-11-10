df8934733ab9c29ebed6756940ee69ceb7708efb
Jatin <jatinj615@gmail.com>
2017-11-10 22:27:03 +0530

    evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 
 
@@ -27,5 +28,44 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, y_train , y_test = train_test_split( features, labels, test_size = 0.3 , random_state = 42 )
+
+clf = DecisionTreeClassifier() 
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print (accuracy_score(pred, y_test))
+
+### evaluation metrics, counting poi and total person in test set 
+poi_count = 0
+person_count = 0
+for poi in pred:
+    
+    person_count += 1
+
+    if poi == 1:
+            
+        poi_count += 1
+
+print (poi_count)
+print (person_count)
+
+
+### evaluation metrics finding true poi
+i = 0 
+true_poi = 0
+for i in range(len(pred)):
+    if pred[i] == 1 and y_test[i] == 1:
+        true_poi += 1
+
+print (true_poi)
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+print (precision_score(y_test, pred))
+print (recall_score(y_test, pred))
 
 
